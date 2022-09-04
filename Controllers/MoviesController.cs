7e7249fad5f9aa7b8005aeaf1b0db58fb3d19492
Jaxa Mokoto <Jaxa.Mokoto@arcinfra.com>
2022-09-04 16:53:12 +0800

    setting up basic model and controller for Vidly

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            // create an instance of the move module
+            Movie movie = new Movie()
+            {
+                Name = "Shrek"
+            };
+
+            //Render the view model
+            return View(movie);
+        }
+    }
+}
